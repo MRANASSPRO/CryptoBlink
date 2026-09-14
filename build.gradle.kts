2e4950b9176b56cf8818555c3a26fca3b7621d7c
MRANASSPRO <25827669+MRANASSPRO@users.noreply.github.com>
2026-09-14 16:06:12 +0200

    build: add Android CI pipeline with ktlint check

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.dagger.hilt.android) apply false
+    alias(libs.plugins.ktlint) apply false
 }
 
 tasks.register<Delete>("clean") {
