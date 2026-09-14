3d154f82218cbeb119816a771eb22e489c1712ff
MRANASSPRO <25827669+MRANASSPRO@users.noreply.github.com>
2026-09-14 16:19:52 +0200

    build: update ktlint version and configure KtlintExtension in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,35 @@
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.dagger.hilt.android) apply false
-    alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.ktlint)
 }
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+val ktlintVersion = libs.versions.ktlint
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    extensions.configure<KtlintExtension> {
+        version.set(ktlintVersion)
+        ignoreFailures.set(false)
+        outputToConsole.set(true)
+        reporters {
+            reporter(ReporterType.PLAIN)
+            reporter(ReporterType.HTML)
+        }
+        filter {
+            exclude { it.file.path.contains("${File.separator}build${File.separator}") }
+        }
+    }
 }
+
+/*tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}*/
 
 
